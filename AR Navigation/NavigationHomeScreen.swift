dfcc84b035c454ef3c3ed9b4f3ea74f2632682be
Reynard Hansel <92787298+ReynardHansel@users.noreply.github.com>
2025-05-15 14:07:12 +0700

    Cleaned NavigationHomeScreen from bloated comments from original project

--- AR Navigation/NavigationHomeScreen.swift
+++ AR Navigation/NavigationHomeScreen.swift
@@ -26,27 +26,14 @@ struct NavigationHomeScreen: View {
     @State private var cameraPosition: MapCameraPosition = .userLocation(
         fallback: .automatic)
 
-    // Search State
-//    @State private var searchText = ""
     @State private var selectedDestination: Destination? = nil
-//    @FocusState private var isSearchBarFocused: Bool
     
     @State var showModal : Bool = false
 
     // Access / import destination data (from: Destination.swift)
     let destinations = showcaseDestination//destinationDBShowcase
 
-//    var filteredDestinations: [Destination] {
-//        if searchText.isEmpty { return destinations }  //* --> returns all list (no filter)
-//
-//        return destinations.filter {
-//            $0.name.localizedCaseInsensitiveContains(searchText)
-//        }  //* --> returns filtered list based on search value
-//    }
-    //* --> This is a computed property. LEARN MORE ABOUT THIS!. Intinya yg gw tangkep: provides a getter and setter (optional)
-
     var body: some View {
-//        ZStack {
             // Map as background
             Map(position: $cameraPosition) {
                 UserAnnotation()
@@ -59,14 +46,6 @@ struct NavigationHomeScreen: View {
                 ForEach(destinations) { destination in
                     Marker(destination.name, systemImage: destination.icon, coordinate: destination.destinationCoordinate)
                 }
-                
-                // delete later
-//                if let destination = selectedDestination,
-//                   let currentLocation = locationDataManager.locationManager.location?.coordinate
-//                {
-//
-//                }
-                
             }
             .mapControls {
                 MapUserLocationButton()
@@ -77,18 +56,7 @@ struct NavigationHomeScreen: View {
 //                locationManager.requestLocation()
             }
             .sheet(isPresented: $isBottomSheetOpen) {
-//                List(1...10, id: \.self) { n in
-//                    Text("Row \(n)")
-//                        .listRowBackground(
-//                            RoundedRectangle(cornerRadius: 10)
-//                                .strokeBorder(.blue, lineWidth: 2)
-//                                .padding(.top, n == 1 ? 0 : -15)
-//                                .padding(.bottom, n == 10 ? 0 : -15)
-//                                .clipShape(Rectangle())
-//                        )
-//                }
-//                .presentationDetents([.fraction(0.1), .medium, .large])
-//                .interactiveDismissDisabled()
+
                 BottomSheet_Home()
                     .presentationDetents([.fraction(0.1), .medium, .large])
                     .presentationDragIndicator(.visible)
@@ -98,142 +66,9 @@ struct NavigationHomeScreen: View {
                     .presentationBackground(Color.background)
             }
 
-//            VStack {
-//                // Search bar
-//                ZStack{
-//                    HStack {
-//                        Image(systemName: "magnifyingglass")
-//                            .foregroundColor(.gray)
-//                        TextField(
-//                            "Search Destination",
-//                            text: $searchText,
-//                            prompt: Text("Search Destination")
-//                                .foregroundStyle(Color.gray)
-//                        )
-//                        .focused($isSearchBarFocused)
-//                        .onChange(of: searchText, { oldValue, newValue in
-//                            
-//                            if(selectedDestination != nil && selectedDestination?.name != searchText){
-//                                showModal = false
-//                                pathFindingManager.ResetPathfinder()
-//                            }
-//                        })
-//                        .foregroundStyle(Color.black)
-//                        
-//                        if !searchText.isEmpty
-//                        {
-//                            Button(action:{
-//                                self.searchText = ""
-//                                showModal = false
-//                                pathFindingManager.ResetPathfinder()
-//                            })
-//                            {
-//                                Image(systemName: "multiply.circle")
-//                                    .foregroundColor(Color(UIColor.opaqueSeparator))
-//                                    .padding(.trailing, 4)
-//                                    .padding(.vertical, 0)
-//                                    .contentShape(Rectangle())
-//                            }
-//                        }
-//                    }
-//                    .padding()
-//                    .background(Color.white)
-//                    .cornerRadius(50)
-//                    .padding(.top, 10)
-//                    
-//                    
-//                }
-//                
-//                if isSearchBarFocused || !searchText.isEmpty {
-//                    SearchResult(
-//                        destinations: filteredDestinations,
-//                        selectedDestination: $selectedDestination,
-//                        pathFindingManager: pathFindingManager,
-//                        locationDataManager: locationDataManager,
-//                        showModal: $showModal,
-//                        fieldText: $searchText,
-//                        cameraPosition: $cameraPosition,
-//                        isSearchBarFocused: $isSearchBarFocused
-//                    )
-//                }
-//
-//                Spacer()
-//                
-//                // Confirm Destination Modal
-//                if showModal {
-////                    ConfirmDestinationModal(
-////                        showModal: $showModal,
-////                        path: $path,
-////                        onCancel: {
-////                            searchText = ""
-////                        },
-////                        onConfirm: {
-////                            searchText = ""
-////                        },
-////                        destName: selectedDestination?.name ?? "",
-////                        distance: String(format:"%.0f",pathFindingManager.estimateCumulativeDistance),
-////                        time: String(format:"%.0f",pathFindingManager.estimateCumulativeDistance * 0.3)
-////                        
-////                    )
-//                    SelectedDestinationModal(
-//                        showModal: $showModal,
-//                        onCancel: {
-//                            pathFindingManager.ResetPathfinder()
-//                            searchText = ""
-//                            showModal = false
-//                        },
-//                        onConfirm: {
-//                            selectedDestination = nil
-//                            searchText = ""
-//                            path.append("ARView")
-//                        },
-//                        destName:selectedDestination?.name ?? "",
-//                        distance: String(format:"%.0f",pathFindingManager.estimateCumulativeDistance),
-//                        time: String(format:"%.0f",pathFindingManager.estimateCumulativeDistance * 0.3)
-//                    )
-//                }
-//                
-//            }
-//            .padding(.horizontal)
-            
-                    
-//        }
-//        .onTapGesture { isSearchBarFocused = false }
+
     }
 }
-
-
-// LocationManager Class
-//class LocationManager: NSObject, ObservableObject {
-//    @Published var lastLocation: CLLocation?
-//
-//    private let locationManager = CLLocationManager()
-//
-//    override init() {
-//        super.init()
-//        locationManager.delegate = self
-//    }
-//
-//    func requestLocation() {
-//        locationManager.requestWhenInUseAuthorization()
-//        locationManager.startUpdatingLocation()
-//    }
-//}
-//
-//extension LocationManager: CLLocationManagerDelegate {
-//    func locationManager(
-//        _ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]
-//    ) {
-//        guard let location = locations.first else { return }
-//        lastLocation = location
-//    }
-//
-//    func locationManager(
-//        _ manager: CLLocationManager, didFailWithError error: Error
-//    ) {
-//        print("Failed to find user's location: \(error.localizedDescription)")
-//    }
-//}
 
 #Preview {
     NavigationHomeScreen(
